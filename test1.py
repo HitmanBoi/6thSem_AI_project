16c73ca22952298bcd186ddb6069aabeb6e0d024
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-05-21 22:40:45 +0530

    st

--- test1.py
+++ test1.py
@@ -5,7 +5,7 @@ import streamlit as st
 from PIL import Image 
 
 # loading in the model to predict on the data 
-pickle_in = open('classifier.pkl', 'rb') 
+pickle_in = open('model.pkl', 'rb') 
 classifier = pickle.load(pickle_in) 
 
 def welcome(): 
@@ -13,24 +13,21 @@ def welcome():
 
 # defining the function which will make the prediction using 
 # the data which the user inputs 
-def prediction(sepal_length, sepal_width, petal_length, petal_width): 
-
-	prediction = classifier.predict( 
-		[[sepal_length, sepal_width, petal_length, petal_width]]) 
-	print(prediction) 
-	return prediction 
+def prediction(): 
+	a = result.plot
+	return a
 	
 
 # this is the main function in which we define our webpage 
 def main(): 
 	# giving the webpage a title 
-	st.title("Iris Flower Prediction") 
+	st.title("Crypto Price Prediction") 
 	
 	# here we define some of the front end elements of the web page like 
 	# the font and background color, the padding and the text to be displayed 
 	html_temp = """ 
-	<div style ="background-color:yellow;padding:13px"> 
-	<h1 style ="color:black;text-align:center;">Streamlit Iris Flower Classifier ML App </h1> 
+	<div style ="background-color:cyan;padding:13px"> 
+	<h1 style ="color:black;text-align:center;">Crypto Price Prediction ML App</h1> 
 	</div> 
 	"""
 	
@@ -40,17 +37,20 @@ def main():
 	
 	# the following lines create text boxes in which the user can enter 
 	# the data required to make the prediction 
-	sepal_length = st.text_input("Sepal Length", "Type Here") 
-	sepal_width = st.text_input("Sepal Width", "Type Here") 
-	petal_length = st.text_input("Petal Length", "Type Here") 
-	petal_width = st.text_input("Petal Width", "Type Here") 
-	result ="" 
+	option = st.selectbox(
+        "Select the coin whose price you want to predict?",
+        ("ETH", "BTC", "ICP"),
+        #label_visibility=st.session_state.visibility,
+        #disabled=st.session_state.disabled,
+	)
+
+	
 	
 	# the below line ensures that when the button called 'Predict' is clicked, 
 	# the prediction function defined above is called to make the prediction 
 	# and store it in the variable result 
 	if st.button("Predict"): 
-		result = prediction(sepal_length, sepal_width, petal_length, petal_width) 
+		result = prediction
 	st.success('The output is {}'.format(result)) 
 	
 if __name__=='__main__': 
